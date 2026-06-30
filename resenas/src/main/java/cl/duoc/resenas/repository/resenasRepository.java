dba7472e7798025ce83b0db3bae262965bcf380f
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-06-30 11:44:05 -0400

    añadido opciones de busquedas al repositorio

--- resenas/src/main/java/cl/duoc/resenas/repository/resenasRepository.java
+++ resenas/src/main/java/cl/duoc/resenas/repository/resenasRepository.java
@@ -1,6 +1,23 @@
 package main.java.cl.duoc.resenas.repository;
 
-@Repository
-public interface resenasRepository extends JpaRespository<resenas, UUID> {
+import main.java.cl.duoc.resenas.model.resenasModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+
+@Repository
+public interface resenasRepository extends JpaRespository<resenasModel, UUID> {
+    Optional<resenasModel> findByDestinationIdAndUsersId(UUID destinationsId, UUID usersId);
+
+    List<resenasModel> findAllByUsersId(UUID usersId);
+
+    List<resenasModel> findAllByDestinationsId(UUID destinationsId);
+
+    @org.springframework.data.jpa.repository.Query(
+        "SELECT AVG(r.puntaje) FROM resenas r WHERE r.destinationsId = :destinationsId")
+    Double findAveragePuntajeByDestinationsId(UUID destinationsId);
 }
--- resenas/src/main/java/cl/duoc/resenas/service/resenasService.java
+++ resenas/src/main/java/cl/duoc/resenas/service/resenasService.java
@@ -1,3 +1,31 @@
+package main.java.cl.duoc.resenas.service;
+
+import main.java.cl.duoc.resenas.model.resenasModel;
+import main.java.cl.duoc.resenas.dto.resenasRequest;
+import main.java.cl.duoc.resenas.dto.resenasResponse;
+import main.java.cl.duoc.resenas.model;
+import main.java.cl.duoc.resenas.repository.resenasRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
 public class resenasService {
+
+    private final resenasService resenasService;
+
+private resenasResponse mapToResponse(resenasModel resenasModel){
+    return resenasResponse.builder()
+            .id(resenasModel.getId())
+            .usersId(resenasModel.getUsersId())
+            .destinationsId(resenasModel.getDestinationsId())
+            .puntaje(resenasModel.getPuntaje())
+            .comentario(resenasModel.getComentario())
+            .creacionComen(resenasModel.getCreacionComen())
+            .actualizaComen(resenasModel.getActualizaComen())
+            .build();
+    }
+public resenasResponse crearOActualizar (UUID usersId,)
 
 }
