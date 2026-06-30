b505d5491cc6f847de052aff2ac6eb358319da3d
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-06-30 11:22:15 -0400

    añadido el request y response de resenas

---
+++ resenas/src/main/java/cl/duoc/resenas/dto/resenasRequest.java
@@ -0,0 +1,33 @@
+package main.java.cl.duoc.resenas.dto;
+
+import jakarta.validation.constraint.Max;
+import jakarta.validation.constraint.Min;
+import jakarta.validation.constraint.NotNull;
+import jakarta.validation.constraint.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Builder;
+import java.util.UUID;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class resenasRequest {
+
+    @NotNull(message = "usersId no puede ser nulo")
+    private UUID usersId;
+
+    @NotNull(message = "destinationsId no puede ser nulo")
+    private UUID destinationsId;
+
+    @NotNull(message = "puntaje no puede estar vacio")
+    @Min(value = 1, message = "puntaje debe ser de almenos 1")
+    @Max(value = 10, message = "puntaje no debe exceder 10")
+    private Integer puntaje;
+
+    @Size(max = 1500, message = "comentario no debe exceder las 1500 letras")
+    private String comentario;
+
+}
---
+++ resenas/src/main/java/cl/duoc/resenas/dto/resenasResponse.java
@@ -0,0 +1,32 @@
+package main.java.cl.duoc.resenas.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+import lombok.Builder;
+
+import java.time.Instant;
+import java.util.UUID;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConsturctor
+@Builder
+public class resenasResponse {
+
+    private UUID id;
+
+    private UUID destinationsId;
+
+    private UUID usersId;
+
+    private Integer puntaje;
+
+    private String comentario;
+
+    private Instant CreacionComen;
+    
+    private Instant ActualizaComen;
+
+}
