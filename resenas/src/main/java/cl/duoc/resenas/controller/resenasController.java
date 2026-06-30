f1ad09aedce92d618236eba16350dcac7c84713d
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-06-30 12:02:11 -0400

    añadido lo inicial de controller, falto trabajar en service

---
+++ resenas/src/main/java/cl/duoc/resenas/controller/resenasController.java
@@ -0,0 +1,29 @@
+package main.java.cl.duoc.resenas.controller;
+import main.java.cl.duoc.resenas.dto.resenasRequest;
+import main.java.cl.duoc.resenas.dto.resenasResponse;
+import main.java.cl.duoc.resenas.service.resenasService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/resenas")
+@RequiredArgsConstructor
+public class resenasController {
+
+}
